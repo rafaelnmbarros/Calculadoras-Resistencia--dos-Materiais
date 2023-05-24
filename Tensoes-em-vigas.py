2aa574b96475a04f795bdb48ee33c0518fc4bf7f
Rafael Barros <85089186+rafaelnmbarros@users.noreply.github.com>
2023-05-24 09:47:49 -0400

    _

--- Tensoes-em-vigas.py
+++ Tensoes-em-vigas.py
@@ -1,51 +1,62 @@
+"""
+###############################################################################
+#                          RESISTÊNCIA DOS MATERIAIS                  _y|__   #
+#                           Professor Rafael Barros                  |  |  |  #
+#                                                                  z_|__|  |  #
+#              TENSÕES EM SEÇÕES SOB MOMENTO FLETOR E FORÇA AXIAL    |     |  #
+#                                                                    |_____|  #
+#                                                                             #
+###############################################################################
+"""
+
+
 #%%
-#===============================================================================
+#==============================================================================
 #SEÇÃO RETANGULAR
-#===============================================================================
+#==============================================================================
 #Funções utilizadas
 from FuncoesFlexao import TensFlexRet
 
 #Dados de entrada
 b = 15 #cm (largura da base)
 h = 35 #cm (altura)
-Mz = 11 #kNm (momento em torno de z: +compressão no topo, -compressão na base)
-My = 0 #kNm (momento em torno de y: +compressão à direita, -compressão à esquerda)
-N = -110 #kN (Esforço normal na seção: +tração, -compressão)
+Mz = 11 #kNm (momento em z: +compressão no topo, -comp na base)
+My = 0 #kNm (momento em y: +comp à direita, -comp à esquerda)
+N = -110 #kN (esforço normal: +tração, -compressão)
 
 #Cálculos
 TensFlexRet(b, h, Mz, My, N)
 # %%
-#===============================================================================
+#==============================================================================
 #SEÇÃO EM I
-#===============================================================================
+#==============================================================================
 
 from FuncoesFlexao import TensFlexI
 
-d = 30 #cm
-bf = 20 #cm
-tw = 1.5
-tf = 1.5
-P=120 #kN
-Mz = 2*P*0.25 #kNm (momento em torno de z: +compressão no topo, -compressão na base)
-My = P*((75+7.5)/1000) #kNm (momento em torno de y: +compressão à direita, -compressão à esquerda)
-N = -25 #kN
+d = 30 #cm (altura total)
+bf = 20 #cm (largura da mesa)
+tw = 1.5 #cm (espessura da alma)
+tf = 1.5 #cm (espessura da mesa)
+Mz = 60 #kNm (momento em z: +compressão no topo, -comp na base)
+My = 70 #kNm (momento em y: +comp à direita, -comp à esquerda)
+N = -25 #kN (esforço normal: +tração, -compressão)
 
 TensFlexI(d, bf, tf, tw, Mz, My, N)
 #%%
-##==============================================================================
+##=============================================================================
 #SEÇÃO EM C
-#===============================================================================
+#==============================================================================
 from FuncoesFlexao import TensFlexC
 
-d = 20.3
-bf = 5.94
-tw = 0.77
-tf = 0.991
-Mz = 0.71 #kNm (momento em torno de z: +compressão no topo, -compressão na base)
-My = 0.26 #kNm (momento em torno de y: +compressão à direita, -compressão à esquerda)
-N = 0 #kN
+d = 20.3 #cm (altura total)
+bf = 5.94 #cm (largura da mesa)
+tw = 0.77 #cm (espessura da alma)
+tf = 0.991 #cm (espessura da mesa)
+Mz = 0.71 #kNm (momento em z: +compressão no topo, -comp na base)
+My = 0.26 #kNm (momento em y: +comp à direita, -comp à esquerda)
+N = 0 #kN (esforço normal: +tração, -compressão)
 
 TensFlexC(d, bf, tw, tf, Mz, My, N)
-#%%#===============================================================================
+#%%#===========================================================================
 #SEÇÃO EM L
-#===============================================================================
+#==============================================================================
